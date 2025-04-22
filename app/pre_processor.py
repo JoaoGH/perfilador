460634aed1cdf24bed4c546f56e1448dffdf547c
JoaoGH <gabrielhartmannqwert@gmail.com>
2025-04-22 09:16:11 -0300

    fix: usar ascii

--- app/pre_processor.py
+++ app/pre_processor.py
@@ -22,7 +22,7 @@ class PreProcessor:
 
     def normalize(self, text: str) -> str:
         text = unicodedata.normalize("NFKC", text)
-        text = text.encode("ascii", "ignore").decode("utf-8")
+        text = text.encode("ascii", "ignore").decode("ascii")
         text = re.sub(r"[^\w\s]", "", text)
         return text
 
